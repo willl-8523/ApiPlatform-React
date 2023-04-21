16d693dbf203135122fbf26fe74825c33e6bba7a
willl85 <ngowilfried@gmail.com>
2023-04-21 16:41:26 +0200

    Create User entity

--- migrations/Version20230421143736.php
+++ migrations/Version20230421143736.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230421141357 extends AbstractMigration
+final class Version20230421143736 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Ajouter la propriete chrono dans la table invoices';
+        return 'Create user table and put all table in plural';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE invoices ADD chrono INT NOT NULL');
+        $this->addSql('ALTER TABLE customers ADD CONSTRAINT FK_62534E21A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE invoices DROP chrono');
+        $this->addSql('ALTER TABLE customers DROP FOREIGN KEY FK_62534E21A76ED395');
     }
 }
--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -45,6 +45,11 @@ class Customer
      */
     private $invoices;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+     */
+    private $user;
+
     public function __construct()
     {
         $this->invoices = new ArrayCollection();
@@ -129,6 +134,18 @@ class Customer
                 $invoice->setCustomer(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
