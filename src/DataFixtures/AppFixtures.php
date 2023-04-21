1ba7b39081e76c2279ec91e3a975b8c5c97276cb
willl85 <ngowilfried@gmail.com>
2023-04-21 17:14:41 +0200

    Add data in table user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,48 +6,81 @@ use DateInterval;
 use DateTimeImmutable;
 use App\Entity\Invoice;
 use App\Entity\Customer;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * Le hacheur de mot de passe
+     *
+     * @var UserPasswordHasherInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create();
+
         $today = new DateTimeImmutable();
         $interval = new DateInterval('P6M'); // P6M représente une période de 6 mois
         $minDate = $today->sub($interval)->getTimestamp(); // la date minimum est aujourd'hui moins 6 mois
         $maxDate = $today->getTimestamp(); // la date maximum est aujourd'hui
 
-        $chrono = 1;
         
-        for ($c = 0; $c < 30; $c++) { 
-            $customer = new Customer;
-            $customer->setFirstName($faker->firstName())
-                     ->setLastName($faker->lastName())
-                     ->setCompany($faker->company)
-                     ->setEmail($faker->email);
-                     
-            $manager->persist($customer);
+        // Créer 10 utilisateurs
+        for ($u = 0; $u < 10; $u++) { 
+            $user = new User;
+            
+            $chrono = 1; //permet d'identifier une facture
+            $hash = $this->encoder->hashPassword($user, "secret");
 
-            // Generer entre 3 et 10 facture
-            for ($i = 0; $i < mt_rand(3, 10) ; $i++) { 
-                $invoice = new Invoice;
+            $user->setFirstName($faker->firstName)
+                 ->setLastName($faker->lastName)
+                 ->setEmail($faker->email)
+                 ->setPassword($hash);
 
-                $randomTimestamp = mt_rand($minDate, $maxDate); // génère un timestamp aléatoire entre la date minimum et la date maximum
+            $manager->persist($user);
 
-                $randomDate = new DateTimeImmutable();
-                $randomDate = $randomDate->setTimestamp($randomTimestamp); // crée un objet DateTimeImmutable à partir du timestamp aléatoire
-
-                $invoice->setAmount($faker->randomFloat(2, 250, 5000))
-                        ->setSentAt($randomDate) 
-                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED'])) // récupere un element du tableau
-                        ->setCustomer($customer) // Attribuer la facture au client
-                        ->setChrono($chrono);
-
-                $manager->persist($invoice);
-
-                $chrono++;
+            // Créer entre 5 et 20 clients
+            for ($c = 0; $c < mt_rand(5, 20); $c++) { 
+                $customer = new Customer;
+                $customer->setFirstName($faker->firstName)
+                         ->setLastName($faker->lastName)
+                         ->setCompany($faker->company)
+                         ->setEmail($faker->email)
+                         ->setUser($user);
+                         
+                $manager->persist($customer);
+    
+                // Generer entre 3 et 10 facture
+                for ($i = 0; $i < mt_rand(3, 10) ; $i++) { 
+                    $invoice = new Invoice;
+    
+                    $randomTimestamp = mt_rand($minDate, $maxDate); // génère un timestamp aléatoire entre la date minimum et la date maximum
+    
+                    $randomDate = new DateTimeImmutable();
+                    $randomDate = $randomDate->setTimestamp($randomTimestamp); // crée un objet DateTimeImmutable à partir du timestamp aléatoire
+    
+                    $invoice->setAmount($faker->randomFloat(2, 250, 5000))
+                            ->setSentAt($randomDate) 
+                            ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED'])) // récupere un element du tableau
+                            ->setCustomer($customer) // Attribuer la facture au client
+                            ->setChrono($chrono);
+    
+                    $manager->persist($invoice);
+    
+                    $chrono++;
+                }
             }
         }
 
