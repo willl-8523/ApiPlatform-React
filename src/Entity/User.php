d98edbf0656da409e8c9f4b942ea89ffe722e20f
willl85 <ngowilfried@gmail.com>
2023-05-04 11:27:20 +0200

    Extraction des données des Invoices et des Customers liées aux Users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
@@ -21,16 +22,19 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"invoices_read", "customer_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"invoices_read", "customer_read"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @Groups({"invoices_read", "customer_read"})
      */
     private $roles = [];
 
@@ -42,11 +46,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read", "customer_read"})
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read", "customer_read"})
      */
     private $lastName;
 
