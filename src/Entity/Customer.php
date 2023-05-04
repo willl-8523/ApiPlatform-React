d81de128efa474d48d61fd7afdcb6975a0d9870c
willl85 <ngowilfried@gmail.com>
2023-05-04 15:41:39 +0200

    Les sous-ressource

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -7,6 +7,7 @@ use App\Repository\CustomerRepository;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
@@ -72,6 +73,7 @@ class Customer
     /**
      * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="customer")
      * @Groups({"customer_read"})
+     * @ApiSubresource
      */
     private $invoices;
 
