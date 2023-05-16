e7700812047374d304673ffaf344a21026b2a846
willl85 <ngowilfried@gmail.com>
2023-05-16 16:30:33 +0200

    Intervenir sur la création d'une invoice pour lui donner un chrono

---
+++ src/Events/InvoiceChronoSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Events;
+
+use DateTimeImmutable;
+use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class InvoiceChronoSubscriber implements EventSubscriberInterface
+{
+
+    private $security;
+    private $repo;
+
+    public function __construct(Security $security, InvoiceRepository $repo)
+    {
+        $this->security = $security;
+        $this->repo = $repo;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setChronoForInvoice', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setChronoForInvoice(ViewEvent $event) {
+
+        /**
+         * Trouver l'utilisateur actuellement connecté (security)
+         * Récuperer le repo des factures (InvoiceRepository)
+         * Récuperer la derniere facture qui a été inserée pour avoir son chrono
+         * Dans la facture créee, on donne le dernier chrono + 1
+         */
+        
+        $invoice = $event->getControllerResult();
+        // dd($invoice);
+        $method = $event->getRequest()->getMethod();
+
+        /** @var User $user */
+        $user = $this->security->getUser();
+
+        if($user !== $invoice->getCustomer()->getUser()) {
+            throw new AccessDeniedHttpException("Ce client ne vous appartient pas, merci de vous rapprocher de l'utilisateur auquel il se rapporte.");
+        }
+
+        if ($invoice instanceof Invoice && $method === "POST") {
+            $nextChrono = $this->repo->findNextChrono($this->security->getUser());
+            $invoice->setChrono($nextChrono);
+
+            $invoice->setSentAt(new DateTimeImmutable("now"));
+        }
+
+    }
+}
--- src/Repository/InvoiceRepository.php
+++ src/Repository/InvoiceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Invoice;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -37,6 +38,48 @@ class InvoiceRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+    }
+
+    /**
+     * Trouver le dernier chrono
+     * 
+     * ############# DQL ############
+     * 
+     * $this->createQueryBuilder("i") // faire une requête sur les invoices
+     *                                  alias i
+     *  ->select("i.chrono") // Selectionné le champ chrono des invoices
+     *  ->join("i.customer", "c") // Joindre le customer (c) lié aux invoices
+     *  ->where("c.user = :user") // Prendre les invoices qui ont pour 
+     *                                   customer un utilisateur = :user (qui 
+     *                                   est 1e variable qui sera remplacé)
+     *  ->setParameter("user", $user) // :user = $user
+     *  ->orderBy("i.chrono", "DESC") // ordonner par chrono decroissant
+     *  ->setMaxResults(1) // Prendre le dernier
+     *  ->getQuery()
+     *  ->getSingleScalarResult() + 1; // prendre uniquement le nombre qui
+     *                                    correspond au chrono + 1
+     * 
+     * ############# SQL ############
+     * 
+     *  SELECT chrono FROM `invoices`
+     *  JOIN customers ON invoices.customer_id = customers.id
+     *  WHERE user_id = :user (31) 
+     *  ORDER BY chrono DESC LIMIT 1;
+     *
+     * @param User $user
+     * @return void
+     */
+    public function findNextChrono(User $user) 
+    {
+        return $this->createQueryBuilder("i")
+            ->select("i.chrono")
+            ->join("i.customer", "c")
+            ->where("c.user = :user")
+            ->setParameter("user", $user)
+            ->orderBy("i.chrono", "DESC")
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleScalarResult() + 1;
     }
 
 //    /**
