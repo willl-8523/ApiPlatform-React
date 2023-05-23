c1314387ce20df65d4dede849cb658c43da1511a
willl85 <ngowilfried@gmail.com>
2023-05-23 16:53:38 +0200

    Création de la page CustomersPage

--- assets/js/pages/CustomersPage.jsx
+++ assets/js/pages/CustomersPage.jsx
@@ -2,8 +2,41 @@ import React from 'react';
 
 
 const CustomersPage = () => {
-    return ( 
+    return (
+      <>
         <h1>Liste des clients</h1>
+
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Client</th>
+              <th>Email</th>
+              <th>Entreprise</th>
+              <th className="text-center">Factures</th>
+              <th className="text-center">Montant total</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>18</td>
+              <td>
+                <a href="#">Ari Boulogne</a>
+              </td>
+              <td>ari@example.com</td>
+              <td>Ari Inc</td>
+              <td className="text-center">
+                <span class="badge badge-dark px-2 py-1">4</span>
+              </td>
+              <td className="text-center">2 400.00 €</td>
+              <td>
+                <button className="btn btn-sm btn-danger">Supprimer</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </>
     );
 }
  
