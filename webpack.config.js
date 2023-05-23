ed603a39ca15857d9e53ee8f5023f92111855971
willl85 <ngowilfried@gmail.com>
2023-05-23 10:59:52 +0200

    Creation de la page d'acceuil qui portera l'application React

--- webpack.config.js
+++ webpack.config.js
@@ -46,9 +46,9 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // configure Babel
-    .configureBabel((config) => {
-        config.plugins.push('@babel/a-babel-plugin');
-    })
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
 
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
