50c3f1a16edf46eaa464bc3e7148e3566d2444b8
willl85 <ngowilfried@gmail.com>
2023-06-24 11:19:22 +0200

    Installation de package dovenv et modification des variables d'environnement

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
-export const API_URL = 'https://localhost:8000/api/';
+// process.env.API_URL => pour avoir la variable d'environnement
+export const API_URL = process.env.API_URL;
 
-export const CUSTOMERS_API = API_URL + "customers";
-export const INVOICES_API = API_URL + "invoices";
-export const USERS_API = API_URL + "users";
-export const LOGIN_API = API_URL + "login_check";
+export const CUSTOMERS_API = API_URL + 'customers';
+export const INVOICES_API = API_URL + 'invoices';
+export const USERS_API = API_URL + 'users';
+export const LOGIN_API = API_URL + 'login_check';
