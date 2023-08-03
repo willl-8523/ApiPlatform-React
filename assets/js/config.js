f22559572949667d1d89008ea6b6e8d185c8460b
willl85 <ngowilfried@gmail.com>
2023-08-03 12:52:54 +0200

    Ajout du fichier config.js

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+// process.env.API_URL => pour avoir la variable d'environnement
+export const API_URL = process.env.API_URL;
+
+export const CUSTOMERS_API = API_URL + 'customers';
+export const INVOICES_API = API_URL + 'invoices';
+export const USERS_API = API_URL + 'users';
+export const LOGIN_API = API_URL + 'login_check';
